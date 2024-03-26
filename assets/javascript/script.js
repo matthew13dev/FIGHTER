3c6d1b5b8f43f3fad6e9ab566b2515c2233eb161
Matheus Henrique Mendes <matthew.coder@outlook.com>
2024-03-25 23:16:10 -0300

    add: arena and log system

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,4 @@
-let player1 = new Knight("Luck");
-console.log(player1.getStatus())
 
-let player2 = new Mage("Matthew");
-console.log(player2.getStatus())
 
-let monster1 = new Slime();
-console.log(monster1.getStatus())
-
-let monster2 = new Goblin();
-console.log(monster2.getStatus())
+let stage = new Stage();
+stage.start();
